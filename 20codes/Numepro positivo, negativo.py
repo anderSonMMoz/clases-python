b95708578f5f6e6727706d39cc27fb01ea75db1f
AndyOnixan <anderr2110son@gmail.com>
2025-10-20 09:45:44 -0400

    Positivos y negtivos en 20 codes

---
+++ 20codes/Numepro positivo, negativo.py
@@ -0,0 +1,7 @@
+Numero = int(input("Ingrese un numero: "))
+if Numero > 0:
+    print("El numero es positivo")
+elif Numero < 0:
+    print("El numero es negativo")
+else:
+    print("El numero es cero")
