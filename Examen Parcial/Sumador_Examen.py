edbd6a95902b4c46d30c04e67d9e1f0df4698121
anderr2110 <anderr2110son@gmail.com>
2025-11-20 20:52:22 -0400

    Examen

---
+++ Examen Parcial/Sumador_Examen.py
@@ -0,0 +1,7 @@
+#Escribe un programa que sume todos los números del 1 al 100 usando un ciclo while y muestre el resultado.
+Suma = 0 
+num = 1
+while num <= 100 :
+    Suma += num
+    num += 1
+print("La suma de los numeros del 1 al 100 es : " , Suma)
